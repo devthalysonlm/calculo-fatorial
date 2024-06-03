1fa2c711a82fbf28547401fbdf624a90e7303243
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-06-03 20:39:39 -0300

    index.js
    transferência de botões do HTML  para as funcionalidade JS ok!

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function calcular(){
+    let txtnumber = window.document.querySelector('input#txtnumber')
+    let res = window.document.querySelector('div#res')
+    let num = Number(txtnumber.value)
 
+    if(num <= 0){
+        alert('Número inválido!')
+    } else {
+        alert('Número inserido com sucesso!')
+    }
+
+    res.innerHTML = `O número inserido foi o ${num}`
+    
+
+}
