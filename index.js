9f5512a809f129070b05f66bcb28e4f9f0d13a22
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-06-03 21:52:51 -0300

    index.js
    Lógica de calculo fatorial ok!

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ function calcular(){
     let txtnumber = window.document.querySelector('input#txtnumber')
     let res = window.document.querySelector('div#res')
     let num = Number(txtnumber.value)
-
-    if(num <= 0){
-        alert('Número inválido!')
-    } else {
-        alert('Número inserido com sucesso!')
-    }
-
-    res.innerHTML = `O número inserido foi o ${num}`
     
-
+    if(num >= 0){
+        fat = 1
+        for(let c = num; c > 1; c--){
+            fat *= c
+        }
+        res.innerHTML = `O fatorial de ${num} é ${fat}`
+    } else {
+        alert('[ERRO] Tente novamente!')
+        res.innerHTML = 'Insira um valor válido!'
+    }
+   
+    
 }
